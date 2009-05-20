d9d639bd3e8484dba93f36bbd6f540acdece2502
Jay Hannah <jay@jays.net>
2009-05-20 15:16:10 -0500

    [8436] A bunch of updates per Sean's feedback.

--- graphviz/DR.pl
+++ graphviz/DR.pl
@@ -7,15 +7,17 @@ use GraphViz;
 
 my %normal =  ( fontname => 'Courier', fontsize => 9, shape => 'box', style => 'filled', fillcolor => 'green' );
 my %repl =    ( fontname => 'Courier', fontsize => 9, shape => 'box', style => 'filled', fillcolor => 'yellow' );  # replicated
-my %dr =      ( fontname => 'Courier', fontsize => 9, shape => 'box', style => 'filled', fillcolor => 'red' );
 my %service = ( fontname => 'Courier', fontsize => 9 );
+my %partial = ( %service, label => 'partial', color => 'blue', fontcolor => 'blue' );
 
 my $g = GraphViz->new(
-   layout => 'fdp',
+   # layout => 'fdp',   # force directed spring model
+                        # makes everything kind of radiate out from center
+                        # instead of cascase down
 );
 
 my $dbs = join "\n", qw( aspect chartofaccounts cms crm hours ideal_idea phpbb rt3 webcal webcal3 );
-$g->add_node($dbs, cluster=>'omares-intranet1', %normal);
+$g->add_node($dbs, cluster=>"omares-intranet1\n(deprecate me RT8757)", %normal);
 
 my $dbs = join "\n", qw( phoenix omnihub omnimq );
 $g->add_node($dbs, cluster=>'dev', %normal);
@@ -31,28 +33,31 @@ $g->add_node('omnihub@l3omnihub2', cluster=>'l3omnihub2', %repl);
 $g->add_node('omnimq@l3omnihub2',  cluster=>'l3omnihub2', %repl);
 $g->add_node('phoenix@l3omnihub2', cluster=>'l3omnihub2', %normal);
 
+$g->add_edge('omnimq@l3omnihub1' => 'omnimq@l3omnihub2',        %partial);
+$g->add_edge('omnimq@l3omnihub1' => 'omnimq@omares-omnihub1dr', %partial);
+$g->add_edge('omnimq@l3omnihub1' => 'omnimq@omares-omnihub2dr', %partial);
+
+# -----------------------
 # DR some day?
-$g->add_node('omnihub@dr', cluster=>'dr', %repl);
-$g->add_node('omnimq@dr',  cluster=>'dr', %repl);
-$g->add_node('phoenix@dr', cluster=>'dr', %repl);
-$g->add_edge('omnimq@l3omnihub1' => 'omnimq@dr');
-$g->add_edge('phoenix@l3omnihub2' => 'phoenix@dr');
-$g->add_edge('omnihub@omares-intranet2' => 'omnihub@dr');
+$g->add_node('omnihub@omares-omnihub1dr', cluster=>'omares-omnihub1dr', %repl);
+$g->add_node('omnimq@omares-omnihub1dr',  cluster=>'omares-omnihub1dr', %repl);
+$g->add_node('phoenix@omares-omnihub1dr', cluster=>'omares-omnihub1dr', %repl);
+$g->add_edge('omnihub@omares-intranet2' => 'omnihub@omares-omnihub1dr');
 
-$g->add_node('crm@l3omnihub3', cluster=>'l3omnihub3', %normal);
+$g->add_node('omnihub@omares-omnihub2dr', cluster=>'omares-omnihub2dr', %repl);
+$g->add_node('omnimq@omares-omnihub2dr',  cluster=>'omares-omnihub2dr', %repl);
+$g->add_node('phoenix@omares-omnihub2dr', cluster=>'omares-omnihub2dr', %repl);
+$g->add_edge('omnihub@omares-intranet2' => 'omnihub@omares-omnihub2dr');
+# -----------------------
 
-$g->add_node('crm',      cluster=>'crmqa',      %normal);
+$g->add_node('crm', cluster=>'l3omnihub3', %normal);
 
 $g->add_node('omnihub@omares-intranet2', cluster=>'omares-intranet2', %normal);
 
 $g->add_edge('omnihub@omares-intranet2' => 'omnihub@l3omnihub2');
 $g->add_edge('omnihub@omares-intranet2' => 'omnihub@l3omnihub1');
 
-# Actually, we'll probably won't bother replicating these:
-# $g->add_edge('omnimq@l3omnihub1' => 'omnimq@l3omnihub2');
-# $g->add_edge('phoenix@l3omnihub2' => 'phoenix@l3omnihub1');
-
-my $boxen = "l3phoenixprod1\nl3phoenixprod2\nl3phoenixprod3";
+my $boxen = "Phoenix";
 $g->add_node($boxen, %service);
 $g->add_edge($boxen => 'phoenix@l3omnihub2');
 $g->add_edge($boxen => 'omnihub@l3omnihub2');
@@ -61,6 +66,12 @@ my $boxen = "OmniMQ";
 $g->add_node($boxen, %service);
 $g->add_edge($boxen => 'omnimq@l3omnihub1');
 $g->add_edge($boxen => 'omnihub@l3omnihub1');
+
+$g->add_node('Elmer', %service);
+$g->add_edge('Elmer' => 'crm');
+
+$g->add_node('OmniHUB', %service);
+$g->add_edge('OmniHUB' => 'omnihub@omares-intranet2');
 
 open (PNG, ">DR.png");
 print PNG $g->as_png;
