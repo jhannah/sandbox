f4a2a1458c2c79b471048935454c6898af7e38ed
Jay Hannah <jay@jays.net>
2009-05-22 13:46:09 -0500

    A bunch of new/corrected DR stuff.

--- graphviz/DR.pl
+++ graphviz/DR.pl
@@ -5,10 +5,11 @@
 use strict;
 use GraphViz;
 
-my %normal =  ( fontname => 'Courier', fontsize => 9, shape => 'box', style => 'filled', fillcolor => 'green' );
-my %repl =    ( fontname => 'Courier', fontsize => 9, shape => 'box', style => 'filled', fillcolor => 'yellow' );  # replicated
-my %service = ( fontname => 'Courier', fontsize => 9 );
-my %partial = ( %service, label => 'partial', color => 'blue', fontcolor => 'blue' );
+my %normal =    ( fontname => 'Courier', fontsize => 9, shape => 'box', style => 'filled', fillcolor => 'green' );
+my %repl =      ( fontname => 'Courier', fontsize => 9, shape => 'box', style => 'filled', fillcolor => 'yellow' );  # replicated
+my %service =   ( fontname => 'Courier', fontsize => 9 );
+my %drservice = ( fontname => 'Courier', fontsize => 9, color => 'red', fontcolor => 'red' );   
+my %partial =   ( %service, label => 'partial', color => 'blue', fontcolor => 'blue' );
 
 my $g = GraphViz->new(
    # layout => 'fdp',   # force directed spring model
@@ -28,13 +29,13 @@ $g->add_node($dbs, cluster=>'qa', %normal);
 
 $g->add_node('omnihub@l3omnihub1', cluster=>'l3omnihub1', %repl);
 $g->add_node('omnimq@l3omnihub1',  cluster=>'l3omnihub1', %normal);
-$g->add_node('phoenix@l3omnihub1', cluster=>'l3omnihub1', %repl);
+$g->add_node("phoenix\@l3omnihub1\n(hot spare)", cluster=>'l3omnihub1', %repl);
 
 $g->add_node('omnihub@l3omnihub2', cluster=>'l3omnihub2', %repl);
-$g->add_node('omnimq@l3omnihub2',  cluster=>'l3omnihub2', %repl);
+$g->add_node("omnimq\@l3omnihub2\n(hot spare)",  cluster=>'l3omnihub2', %repl);
 $g->add_node('phoenix@l3omnihub2', cluster=>'l3omnihub2', %normal);
 
-$g->add_edge('omnimq@l3omnihub1' => 'omnimq@l3omnihub2',        %partial);
+$g->add_edge('omnimq@l3omnihub1' => "omnimq\@l3omnihub2\n(hot spare)",        %partial);
 $g->add_edge('omnimq@l3omnihub1' => 'omnimq@omares-omnihubdr1', %partial);
 $g->add_edge('omnimq@l3omnihub1' => 'omnimq@omares-omnihubdr2', %partial);
 
@@ -42,7 +43,6 @@ $g->add_edge('omnimq@l3omnihub1' => 'omnimq@omares-omnihubdr2', %partial);
 # DR some day?
 $g->add_node('omnihub@omares-omnihubdr1', cluster=>'omares-omnihubdr1', %repl);
 $g->add_node('omnimq@omares-omnihubdr1',  cluster=>'omares-omnihubdr1', %repl);
-$g->add_node('phoenix@omares-omnihubdr1', cluster=>'omares-omnihubdr1', %repl);
 $g->add_edge('omnihub@omares-intranet2' => 'omnihub@omares-omnihubdr1');
 
 $g->add_node('omnihub@omares-omnihubdr2', cluster=>'omares-omnihubdr2', %repl);
@@ -51,9 +51,18 @@ $g->add_node('phoenix@omares-omnihubdr2', cluster=>'omares-omnihubdr2', %repl);
 $g->add_edge('omnihub@omares-intranet2' => 'omnihub@omares-omnihubdr2');
 # -----------------------
 
-$g->add_node('crm', cluster=>'l3omnihub3', %normal);
+$g->add_node('Elmer', %service);
+$g->add_node('crm@l3omnihub3',   cluster=>'l3omnihub3', %normal);
+$g->add_node('crm@omares-crmdr', cluster=>'omares-crmdr', %repl);
+$g->add_edge('Elmer' => 'crm@l3omnihub3');
+$g->add_edge('crm@l3omnihub3' => 'crm@omares-crmdr');
+my $boxen = "Elmer DR";
+$g->add_node($boxen, %drservice);
+$g->add_edge($boxen => 'crm@omares-crmdr', %drservice);
 
 $g->add_node('omnihub@omares-intranet2', cluster=>'omares-intranet2', %normal);
+$g->add_node('omnihub@l3intranetdr2',    cluster=>'l3intranetdr2',  %repl);
+$g->add_edge('omnihub@omares-intranet2' => 'omnihub@l3intranetdr2');
 
 $g->add_edge('omnihub@omares-intranet2' => 'omnihub@l3omnihub2');
 $g->add_edge('omnihub@omares-intranet2' => 'omnihub@l3omnihub1');
@@ -63,16 +72,27 @@ $g->add_node($boxen, %service);
 $g->add_edge($boxen => 'phoenix@l3omnihub2');
 $g->add_edge($boxen => 'omnihub@l3omnihub2');
 
+my $boxen = "Phoenix DR";
+$g->add_node($boxen, %drservice);
+$g->add_edge($boxen => 'phoenix@omares-omnihubdr2', %drservice);
+$g->add_edge($boxen => 'omnihub@omares-omnihubdr2', %drservice);
+
 my $boxen = "OmniMQ";
 $g->add_node($boxen, %service);
 $g->add_edge($boxen => 'omnimq@l3omnihub1');
 $g->add_edge($boxen => 'omnihub@l3omnihub1');
 
-$g->add_node('Elmer', %service);
-$g->add_edge('Elmer' => 'crm');
+my $boxen = "OmniMQ DR";
+$g->add_node($boxen, %drservice);
+$g->add_edge($boxen => 'omnimq@omares-omnihubdr1', %drservice);
+$g->add_edge($boxen => 'omnihub@omares-omnihubdr1', %drservice);
 
 $g->add_node('OmniHUB', %service);
 $g->add_edge('OmniHUB' => 'omnihub@omares-intranet2');
+
+my $boxen = "OmniHUB DR";
+$g->add_node($boxen, %drservice);
+$g->add_edge($boxen => 'omnihub@l3intranetdr2', %drservice);
 
 open (PNG, ">DR.png");
 print PNG $g->as_png;
