d7a4eaa85ee12b93fbacc30f950f3167a5286f03
Jay Hannah <jay@jays.net>
2009-07-03 14:04:55 -0500

    ndim kickstart: mutate! woot!

---
+++ erlang/mutate.erl
@@ -0,0 +1,52 @@
+% From ndim on Freenode (#erlang)
+% http://github.com/ndim/erlang_stuff/blob/master/src/deafferret.erl
+
+-module(deafferret).
+ 
+-export([start/0]).
+-export([mutate/1, mutate/2]).
+-export([m/1, m/2]).
+ 
+ 
+%% Mute version
+m([], _Dict, _Prefix, Acc) ->
+    lists:reverse(Acc);
+m([Base|Suffix], Dict, Prefix, Acc) ->
+    m(Suffix, Dict, [Base|Prefix],
+      lists:foldl(fun(El,Ac) -> [El|Ac] end, Acc,
+     [Prefix++[X|Suffix] || X<-Dict, X=/=Base])).
+ 
+m(OrigSequence, Dict) ->
+    m(OrigSequence, Dict, "", []).
+ 
+m(OrigSequence) ->
+    m(OrigSequence, "ACGT").
+ 
+ 
+%% Verbose version
+mutate([], _Dict, _Prefix, Acc) ->
+    lists:reverse(Acc);
+mutate([Base|Suffix]=OrigSequence, Dict, Prefix, Acc) ->
+    io:format("mutate(~p,~p,~p,~p)~n", [OrigSequence,Dict,Prefix,Acc]),
+    NewMutations = [Prefix++[X|Suffix] || X<-Dict, X=/=Base],
+    io:format(" new: ~p~n", [NewMutations]),
+    mutate(Suffix, Dict, [Base|Prefix],
+   lists:foldl(fun(El,Ac) -> [El|Ac] end, Acc, NewMutations)).
+ 
+mutate(OrigSequence, Dict) ->
+    mutate(OrigSequence, Dict, "", []).
+ 
+mutate(OrigSequence) ->
+    mutate(OrigSequence, "ACGT").
+ 
+ 
+%% Run example.
+example(OrigSequence) ->
+    Dict = "ACGT",
+    Mutations = mutate(OrigSequence, Dict),
+    io:format("Mutations of ~p for dictionary ~p:~n ~p~n", [OrigSequence, Dict, Mutations]).
+ 
+start() ->
+    % [ example(X) || X <- ["CATTAG", "AAAA"] ].
+    [ example(X) || X <- [ "AAAA" ] ].
+ 
---
+++ erlang/scratch/shop.erl
@@ -0,0 +1,9 @@
+-module(shop).
+-export([cost/1]).
+
+cost(oranges)   -> 5;
+cost(newspaper) -> 8;
+cost(apples)    -> 2;
+cost(pears)     -> 9;
+cost(milk)      -> 7.
+
