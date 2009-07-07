edd138d21462e619f232ed3a7c8198ecdf235776
Jay Hannah <jay@jays.net>
2009-07-07 15:48:38 -0500

    x.pl is the new shucking and jiving Emos is looking for.

--- emos/fetch.pl
+++ emos/fetch.pl
@@ -1,8 +1,8 @@
 use Bio::Perl;
 
 my $out = Bio::SeqIO->new(-format => 'fasta', -file => '>out.fasta');
-foreach my $id (qw( DISC1 ACE ACTC )) {
-   my $seq = get_sequence('refseq', $id);
+foreach my $id (qw( ENSG00000162946 )) {
+   my $seq = get_sequence('embl', $id);
    $out->write_seq($seq);
 }
 
