c32e3ab9cebb88be955d947a6b6c6fa7a76b67ae
Jay Hannah <jay@jays.net>
2009-07-07 19:06:40 -0500

    More explicit notes

--- emos/ensembl.pl
+++ emos/ensembl.pl
@@ -12,12 +12,19 @@ $registry->load_registry_from_db(
 my $slice_adaptor = $registry->get_adaptor( 'Human', 'Core', 'Slice' );
 
 # This works:
-$slice = $slice_adaptor->fetch_by_region( 'chromosome', '20', 100000, 101000 );
-print $slice->seq;
+#$slice = $slice_adaptor->fetch_by_region( 'chromosome', '20', 100000, 101000 );
+#print $slice->seq;
 
-# These don't:
-#$slice = $slice_adaptor->fetch_by_region( 'supercontig', 'NT_025741.15', 100, 200 );
-# $slice = $slice_adaptor->fetch_by_region( 'supercontig', 'NT_025741');
-# print $slice->seq;
+# This works on the web:
+#    http://www.ensembl.org/Homo_sapiens/Search/Summary?species=all;idx=;q=NT_025741
+# But not through the API?? None of these work:
+#$slice = $slice_adaptor->fetch_by_region('supercontig', 'NT_025741.15');
+#print $slice->seq;
+#$slice = $slice_adaptor->fetch_by_region('contig',      'NT_025741.15');
+#print $slice->seq;
+#$slice = $slice_adaptor->fetch_by_region('supercontig', 'NT_025741');
+#print $slice->seq;
+#$slice = $slice_adaptor->fetch_by_region('contig',      'NT_025741');
+#print $slice->seq;
 
 
