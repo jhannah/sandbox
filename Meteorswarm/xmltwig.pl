a84206573e4eb4755688bbb4c3598d3718863712
Jay Hannah <jay@jays.net>
2009-08-06 13:11:31 -0500

    STDIN instead of parsefile().

--- Meteorswarm/xmltwig.pl
+++ Meteorswarm/xmltwig.pl
@@ -2,10 +2,19 @@
 
 # Little XML::Twig demo for Meteorswarm
 # by Jay Hannah - http://bioperl.org/wiki/User:Jhannah
+#
+# Pipe XML files into this program like so:
+#    cat medicago_chr0_20080103.xml | perl xmltwig.pl
 
+use strict;
 use XML::Twig;
+
+# Grab all STDIN into $xml
+local $/ = undef;
+my $xml = <>;
+
 my $twig = XML::Twig->new();
-$twig->parsefile('medicago_chr0_20080103.xml');
+$twig->parse($xml);
 my $root = $twig->root;
 
 my @x = $root->get_xpath('ASSEMBLY/GENE_LIST/PROTEIN_CODING/TU');
