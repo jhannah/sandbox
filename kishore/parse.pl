1647e07feb9ab24f87c84e6c64a6c41eeaef10b7
Jay Hannah <jay@jays.net>
2010-02-02 12:59:34 -0600

    Adding demo for kishore.

---
+++ kishore/parse.pl
@@ -0,0 +1,22 @@
+use strict;
+use Bio::SearchIO; 
+my $in = new Bio::SearchIO(-format => 'blast', 
+                           -file   => 'blastout.txt');
+while( my $result = $in->next_result ) {
+  ## $result is a Bio::Search::Result::ResultI compliant object
+  while( my $hit = $result->next_hit ) {
+    ## $hit is a Bio::Search::Hit::HitI compliant object
+    while( my $hsp = $hit->next_hsp ) {
+      ## $hsp is a Bio::Search::HSP::HSPI compliant object
+      if( $hsp->length('total') > 50 ) {
+        if ( $hsp->percent_identity >= 75 ) {
+          print "Query=",   $result->query_name,
+            " Hit=",        $hit->name,
+            " Length=",     $hsp->length('total'),
+            " Percent_id=", $hsp->percent_identity, "\n";
+        }
+      }
+    }  
+  }
+}
+
