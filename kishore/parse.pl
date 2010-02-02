ce0efc647c15e8d47a55b2c940b04e681076f38a
Jay Hannah <jay@jays.net>
2010-02-02 13:48:51 -0600

    hit_description()

--- kishore/parse.pl
+++ kishore/parse.pl
@@ -5,16 +5,21 @@ my $in = new Bio::SearchIO(-format => 'blast',
 while( my $result = $in->next_result ) {
   ## $result is a Bio::Search::Result::ResultI compliant object
   while( my $hit = $result->next_hit ) {
+    print "---NEW HIT---\n";
     ## $hit is a Bio::Search::Hit::HitI compliant object
     while( my $hsp = $hit->next_hsp ) {
       ## $hsp is a Bio::Search::HSP::HSPI compliant object
       if( $hsp->length('total') > 50 ) {
         if ( $hsp->percent_identity >= 75 ) {
-          print "Query=",   $result->query_name,
-            " Hit=",        $hit->name,
-            " Length=",     $hsp->length('total'),
-            " Percent_id=", $hsp->percent_identity,
-            " fraq_identical=", $hsp->frac_identical, "\n";
+          print 
+            "Query=",             $result->query_name,        "\n",
+            "Hit=",               $hit->name,                 "\n",
+            "Length=",            $hsp->length('total'),      "\n",
+            "Percent_id=",        $hsp->percent_identity,     "\n",
+            "fraq_identical=",    $hsp->frac_identical,       "\n",
+            "hit_description=",   $hit->description,          "\n",
+            "query_description=", $result->query_description, "\n",
+            "\n\n";
         }
       }
     }  
