11d0bf714fec1c5d8b510598aa59cfa2818a1fa6
Jay Hannah <jay@jays.net>
2010-05-05 19:55:25 -0500

    Full parse run of abooz's .gff, which had errors.

--- abooz/test.pl
+++ abooz/test.pl
@@ -2,13 +2,21 @@ use Bio::FeatureIO::gff;
 use Bio::SeqFeature::Annotated;
 open($fh,"test.gff");
 
-my $featureOut = Bio::FeatureIO->new(
+my $featureIn = Bio::FeatureIO->new(
     -format => 'gff',
     -version => 3,
     -fh => $fh,
     -validate_terms => 0, #boolean. validate ontology terms online?  default 0 (false).
   );
-my $feature = $featureOut->next_feature;
-print $feature->type()."\n";
-print $feature->seq_id()."\n";
+
+while (my $feat_object = $featureIn->next_feature) {
+   print "primary tag: ", $feat_object->primary_tag, "\n";          
+   for my $tag ($feat_object->get_all_tags) {             
+      print "  tag: ", $tag, "\n";             
+      for my $value ($feat_object->get_tag_values($tag)) {                
+         print "    value: ", $value, "\n";             
+      }          
+   }  
+}
+
 
