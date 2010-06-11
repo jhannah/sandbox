8400587a7b322c85c715456a54f1b764eaff0b6b
Jay Hannah <jay@jays.net>
2010-06-10 19:52:03 -0500

    R demo for diconzine

---
+++ diconzine/j.R
@@ -0,0 +1,5 @@
+input = read.table(file='/Users/jhannah/src/sandbox/diconzine/input.txt');
+if(input[2,2] == 15) { input[2,2] = 22 };
+write.table(input, file='/Users/jhannah/src/sandbox/diconzine/output.txt', 
+   row.names=FALSE, col.names=FALSE
+);
---
+++ diconzine/j.sh
@@ -0,0 +1,2 @@
+R CMD BATCH j.R
+
