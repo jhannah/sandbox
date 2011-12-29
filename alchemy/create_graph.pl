49c5e71edd0293a5a650c2642f646b80e87efe6b
Jay Hannah <jhannah@mutationgrid.com>
2011-12-28 18:09:07 -0600

    Looks like it's working.

---
+++ alchemy/create_graph.pl
@@ -0,0 +1,28 @@
+use strict;
+use 5.10.0;
+use GraphViz;
+
+my $g = GraphViz->new();
+open my $out, '>', 'alchemy.png' or die;
+
+my $cnt;
+open my $in, '<', 'combinations.txt' or die;
+<$in>; <$in>;  # discard headers
+while (<$in>) {
+   chomp;
+   s/\d+\. +//;
+   tr/A-Z/a-z/;
+   my ($result, $terms) = split / *= */;
+   my @terms = split / *\+ */, $terms;
+   say "[$result] = [" . (join '],[', @terms) . "]";
+   $g->add_node($result);
+   foreach my $term (@terms) {
+      $g->add_node($term);
+      $g->add_edge($term => $result);
+   }
+   # last if ($cnt++ == 100);
+}
+
+print $out $g->as_png;
+
+
