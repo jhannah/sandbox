a62521ffa048329b53eda355c1cea83172afa01c
Jay Hannah <jay.hannah@iinteractive.com>
2012-07-02 18:56:38 -0500

    Exact match, say some stuff.

--- Shamanoid/doit.pl
+++ Shamanoid/doit.pl
@@ -5,18 +5,18 @@ use DBI;
 
 my $dbh = DBI->connect(...);
 my $sth1 = $dbh->prepare("select * from [urls]");
-my $sth2 = $dbh->prepare("select * from [orgs] where url like ?");
+my $sth2 = $dbh->prepare("select * from [orgs] where url = ?");
 $sth1->execute;
 while (my $row1 = $sth1->fetchrow_hashref) {
    if ($row1->{url}}) {
-      $sth2->execute('%' . $row1->{url} . '%');
+      say "url is " . $row1->{url};
+      $sth2->execute($row1->{url});
       while (my $row2 = $sth2->fetchrow_hashref) {
+         say "   found a match on orgs row " . $row2->{id};
          # ... 
       }
    }
 }
 
-$sth2->finish;
-$sth1->finish;
 $dbh->disconnect;;
 
