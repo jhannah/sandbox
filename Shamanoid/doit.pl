b7103ac9e9448acd612d04409785c1b91c80d6ba
Jay Hannah <jay.hannah@iinteractive.com>
2012-07-02 19:00:45 -0500

    Update too.

--- Shamanoid/doit.pl
+++ Shamanoid/doit.pl
@@ -6,6 +6,7 @@ use DBI;
 my $dbh = DBI->connect(...);
 my $sth1 = $dbh->prepare("select * from [urls]");
 my $sth2 = $dbh->prepare("select * from [orgs] where url = ?");
+my $sth3 = $dbh->prepare("update [urls] set org_id = ?");
 $sth1->execute;
 while (my $row1 = $sth1->fetchrow_hashref) {
    if ($row1->{url}}) {
@@ -13,6 +14,7 @@ while (my $row1 = $sth1->fetchrow_hashref) {
       $sth2->execute($row1->{url});
       while (my $row2 = $sth2->fetchrow_hashref) {
          say "   found a match on orgs row " . $row2->{id};
+         $sth3->execute($row2->{id});
          # ... 
       }
    }
