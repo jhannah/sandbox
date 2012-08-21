a75919c2c40528195cef96ee6e5f52c6542a1c22
Jay Hannah <jay.hannah@iinteractive.com>
2012-08-21 14:44:35 -0500

    Added some earlier suggestions as comments

--- three18ti/first.pl
+++ three18ti/first.pl
@@ -3,5 +3,11 @@ use 5.10.0;
 
 my @in = ('foo   1', 'bar       2');
 my @out = map { /(\S+)/ } @in;
+
+# Cleaner than these, ya?
+#    map { ($_) = split/\s+/, }
+#    map { (split/\s+/)[0] }
+
 say "[" . (join "|", @out) . "]";
- 
+
+
