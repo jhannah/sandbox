cbb2f24773199b63162e66eda5d0d2b8d0260c6e
Jay Hannah <jay.hannah@iinteractive.com>
2013-03-19 16:20:49 -0500

    Huh. Why is find() doing nothing?

--- jack_knife/mech.pl
+++ jack_knife/mech.pl
@@ -1,31 +1,21 @@
-use warnings;
-use strict;
+use 5.12.1;
 use WWW::Mechanize;
-use WWW::Mechanize::Firefox;
 use HTML::TreeBuilder;
 use HTML::TreeBuilder::XPath;
-use List::MoreUtils qw(uniq);
-use DBI;
-use DBD::mysql;
 use Data::Dumper;
-use Win32::Process::List;
-#use DB::single;
+binmode(STDOUT, ":utf8");
 
 #initialiZe objects
-#my $mech = WWW::Mechanize::Firefox->new( launch => 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe', );
 my $tree= HTML::TreeBuilder::XPath->new();
 my $mech = WWW::Mechanize->new();
 
+my $page = $mech->get("http://m.yahoo.com");
+# say Dumper $mech->content();
+$tree->parse_content( $mech->content() ) or die $!;
+# say $tree->as_XML_indented;
 
-
-			
-
-
-				my $page = $mech->get("http://m.yahoo.com");
-				my $num = int(rand(80)) + 8;
-				my $parsed = $tree->parse_content( $mech->content() );
-				#print Dumper $parsed;
-				my $url = $tree->find('//*[@id="defaultFooter"]/div/div[4]/a[1]');
-				print Dumper $url;
+# my $url = $tree->find('//*[@id="defaultFooter"]/div/div[4]/a[1]');
+say Dumper $tree->findnodes('//*[@id="defaultFooter"]/div/div[4]');
+# say Dumper $url;
 
 
