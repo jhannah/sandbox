ac18a4fd492bc497437a6d9a171f6985934446da
Jay Hannah <jay.hannah@iinteractive.com>
2013-04-01 14:28:03 -0500

    Playing around per https://class.coursera.org/progfun-002/lecture/5

--- CourseraScala/sandbox-sbt/sand.scala
+++ CourseraScala/sandbox-sbt/sand.scala
@@ -2,7 +2,28 @@ object Sand {
 
 	def square(x: Double) = x * x
 
-   def sumOfSquares(x: Double, y: Double) = square(x) + square(y)
+	def sumOfSquares(x: Double, y: Double) = square(x) + square(y)
 
+	def abs(x: Int) = if (x >= 0) x else -x
+
+	def loop: Boolean = loop
+
+/* 
+	-----------------------------------------------------------
+	From https://class.coursera.org/progfun-002/lecture/5
+	-----------------------------------------------------------
+So with Booleans this 'and' (&&) implementation would be fine:          */
+	def and1(x: Boolean, y: Boolean) = 
+		if (x) y else false
+/* 
+But that will hang on 
+		and(false, loop) 
+So we change y from "pass by parameter" to "pass by expression" (call-by-name):
+*/
+	def and2(x: Boolean, y: => Boolean) = 
+		if (x) y else false
+// Here's a convenience pointer to the one that works:
+	def and(x: Boolean, y: => Boolean): Boolean = and2(x, y)
+	// -----------------------------------------------------------
 }
 
