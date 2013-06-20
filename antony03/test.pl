8cca132a47f977a41042a909eb87e53fa7feec4b
Jay Hannah <jay.hannah@iinteractive.com>
2013-06-20 11:03:07 -0500

    Match any of a list.

--- antony03/test.pl
+++ antony03/test.pl
@@ -1,6 +1,10 @@
                 #By Antony Vincent#
  
 #!/usr/bin/perl
+
+use strict;
+use warnings;
+use diagnostics;
  
 use Bio::Perl;
 use Bio::SeqIO;
@@ -8,29 +12,24 @@ use IO::String;
 use Bio::SearchIO;
 use Getopt::Long;
  
- 
-        my $filename;
-        my $help;
+my $filename;
+my $help;
  
  
 GetOptions(
-'file=s' => \$filename,
-'help!' => \$help,
-)
-or die "Incorrect usage! Try perl extract.pl -help for an exhaustif help.\n";
-###
-if( $help )
-{ # if start
-print "\n";
-print "One option is required:\n";
-print " -file: Your file in multi-fasta\n";
-} # if help
- 
-else
-{ ## for the else
-mkdir new_db;
-my @taxa_name= "Hafniomonas laevis";
- 
+    'file=s' => \$filename,
+    'help!' => \$help,
+) or die "Incorrect usage! Try perl extract.pl -help for an exhaustif help.\n";
+
+if( $help ) { 
+    print "\n";
+    print "One option is required:\n";
+    print " -file: Your file in multi-fasta\n";
+    exit;
+}
+
+mkdir 'new_db';
+my @find_these= ("A-12345", "B-12345");
  
 my $gb = Bio::SeqIO->new(-file   => "<$filename",
                               -format => "fasta");
@@ -38,16 +37,17 @@ my $gb = Bio::SeqIO->new(-file   => "<$filename",
 my $fa = Bio::SeqIO->new(-file   => ">new_db/$filename",
                               -format => "fasta",
                               -flush  => 0);
- 
-while($seq = $gb->next_seq) {
+
+SEQ:
+while (my $seq = $gb->next_seq) {
     my $id_and_desc = $seq->id . " " . $seq->desc;
-    $fa->write_seq($seq) if ($id_and_desc =~ /B-12345/);
-    # $fa->write_seq($seq) if (grep {$_ eq $id_and_desc} @taxa_name);
+    foreach my $str (@find_these) {
+        if ($id_and_desc =~ /\Q$str\E/) {
+            $fa->write_seq($seq);
+            next SEQ;
+        }
+    }
 }
  
  
  
- 
-} ##for the else
-
-
