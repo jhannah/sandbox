868d555cc6661e1257e6cf5bef4899a7b5fc046e
Jay Hannah <jay.hannah@iinteractive.com>
2014-01-13 20:20:38 -0600

    Added webart's one-liner.

--- webart/sort.sh
+++ webart/sort.sh
@@ -1,2 +1,7 @@
 sort -k 2 in.txt | perl -anE 'say "$F[0] " . localtime($F[1])'
 
+# or 
+
+perl -anE 'push @rows, [$F[0],$F[1]]}{ say $_->[0]." ".localtime($_->[1]) for sort { $a->[1] <=> $b->[1] } @rows' in.txt
+
+
