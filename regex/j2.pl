d60b760f572885135ffd6ef6295264156f950a0d
Jay Hannah <jay.hannah@iinteractive.com>
2014-07-10 14:19:33 -0500

    more tests

--- regex/j2.pl
+++ regex/j2.pl
@@ -9,10 +9,12 @@ my $re = qr/
   \b           # Word boundary
 /ix;           # i for case insensitive, x for comments
 
-ok('J Hannah'    =~ $re, 'J Hannah');
-ok("J O'Hannah"  =~ $re, "J O'Hannah");
-ok("J O'Hannay"  =~ $re, "J O'Hannay");
-ok('Brad Hannah' !~ $re, 'Brad Hannah');
+ok('J Hannah'      =~ $re, 'J Hannah');
+ok('J hannah'      =~ $re, 'J hannah');
+ok('J Hannahnator' !~ $re, 'J Hannahnator');
+ok("J O'Hannah"    =~ $re, "J O'Hannah");
+ok("Hannay"        =~ $re, "Hannay");
+ok('Brad Hannah'   !~ $re, 'Brad Hannah');
 
 done_testing();
 
