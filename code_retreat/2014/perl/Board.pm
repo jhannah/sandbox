062541a45f5ad49e5b3306aa935570c62da92ac7
Jay Hannah <jay.hannah@iinteractive.com>
2014-11-15 11:58:56 -0600

    more

--- code_retreat/2014/perl/Board.pm
+++ code_retreat/2014/perl/Board.pm
@@ -9,7 +9,7 @@ sub go {
   my $new_board = Board->new();
   foreach my $x (keys %{$self->board}) {
     foreach my $y (keys %{$self->board->{$x}}) {
-      $new_board->set_cell($x, $y, $self->will_survive($x, $y));
+      $new_board->set_cell($x, $y, $self->will_live($x, $y));
     }
   }
   return $new_board;
@@ -30,28 +30,38 @@ sub check_cell {
   $self->board->{$x}->{$y};
 }
 
-sub will_survive {
+sub living_neighbor_count {
   my ($self, $x, $y) = @_;
 
-  my $neighbor_count;
+  my $rval = 0; 
 
-  for(my $i = $x-1; $i <= $x+1; ++$i)
-  {
-      for(my $j = $y-1; $j <= $y+1; ++$j)
-      {
-          if($i == $x && $j == $y)
-          {
-              next;
-          }
-
-          if($self->board->{$x}->{$y} == 1)
-          {
-              $neighbor_count += 1;
+  for (my $i = $x-1; $i <= $x+1; ++$i) {
+      for (my $j = $y-1; $j <= $y+1; ++$j) {
+          next if ($i == $x && $j == $y);
+          $DB::single = 1;
+          if($self->board->{$i}->{$j} && $self->board->{$i}->{$j} == 1) {
+              $rval += 1;
           }
       }
   }
-  print("cell at $x,$y has $neighbor_count neighbors\n");
+  print("cell at $x,$y has $rval living neighbors\n");
 }
+
+sub will_live {
+  my ($self, $x, $y) = @_;
+  my $lnc = $self->living_neighbor_count($x, $y);
+  if ($self->check_cell($x, $y)) {
+    # I'm currently alive
+    if ($lnc < 2)  { return 0 }
+    if ($lnc == 2) { return 1 }
+    if ($lnc == 3) { return 1 }
+  } else {
+    # I'm currently dead
+    if ($lnc == 3) { return 1 }
+  }
+  return 0;
+}
+
 
 1;
 
