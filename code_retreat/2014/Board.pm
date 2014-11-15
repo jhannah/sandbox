1130c67453c5e395e7d4205c1ab7a0ad955f8a75
Jay Hannah <jay.hannah@iinteractive.com>
2014-11-15 09:50:34 -0600

    more! :)

--- code_retreat/2014/Board.pm
+++ code_retreat/2014/Board.pm
@@ -2,6 +2,27 @@ package Board;
 
 use Moose;
 
+has 'board' => (is => 'rw', isa => 'HashRef', default => sub { {} });
+
+sub go {
+  1;
+}
+
+sub display {
+  1;
+}
+
+sub set_cell {
+  my ($self, $x, $y, $value) = @_;
+  $self->board->{$x}->{$y} = $value;
+  return 1;
+}
+
+sub check_cell {
+  my ($self, $x, $y) = @_;
+  $self->board->{$x}->{$y};
+}
+
 
 1;
 
