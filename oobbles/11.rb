4787daaee0b984451ef686597a30a46b169934fa
Jay Hannah <jay.hannah@iinteractive.com>
2015-04-18 17:01:21 -0500

    nested if, fools

---
+++ oobbles/11.rb
@@ -0,0 +1,14 @@
+puts "yo, gimme an odd, fool"
+STDOUT.flush
+n = gets.to_i
+m = n % 2
+if "homework requires nested if"
+  if m == 0 
+    puts "that was even BOO #{m}"
+  else 
+    puts "odd. holla. #{m}"
+  end
+end
+
+
+
