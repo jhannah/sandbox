039891a58320a8a80430092653bfdbbdd6b71b89
Jay Hannah <jay.hannah@iinteractive.com>
2022-03-10 13:35:17 -0600

    Use get_xpath instead

--- voltasolaris/xml_2_json.pl
+++ voltasolaris/xml_2_json.pl
@@ -23,11 +23,11 @@ my $root = $twig->root;
 my $order_id = $root->first_child('OrderHeader')->first_child('Id')->text;
 say "Order ID $order_id";
 
-foreach my $line ($root->first_child('Lines')->children('Line')) {
+foreach my $line ($root->get_xpath('Lines/Line')) {
   my $qty = $line->first_child('ExecQty')->text;
   foreach my $item ($line->children('Item')) {
-    foreach my $part_number ($item->first_child('PartNumbers')->children('PartNumber')) {
-      my $partnum = $part_number->first_child('PartNumber')->text;
+    foreach my $part_number ($item->get_xpath('PartNumbers/PartNumber/PartNumber')) {
+      my $partnum = $part_number->text;
       push @{$out_hashref->{lines}}, {
         partnum => $partnum,
         qty     => $qty,
