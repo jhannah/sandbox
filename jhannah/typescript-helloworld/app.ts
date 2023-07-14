f1099f6345816a9c6184513832481793c51c5ffb
Jay Hannah <jay.hannah@iinteractive.com>
2023-07-14 17:54:33 -0500

    Hello world in Typescript

---
+++ jhannah/typescript-helloworld/app.ts
@@ -0,0 +1,9 @@
+// https://www.typescripttutorial.net/typescript-tutorial/typescript-hello-world/
+
+
+let message: string = 'Hello, TypeScript!';
+// create a new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading the document
+document.body.appendChild(heading);
