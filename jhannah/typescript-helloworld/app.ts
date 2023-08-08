aeffb7a8641faeddbc4679d4f9cd710c8f0808bb
Jay Hannah <jay.hannah@iinteractive.com>
2023-08-08 11:15:53 -0500

    flush podcast list (and some Typescript thing I forgot about)

--- jhannah/typescript-helloworld/app.ts
+++ jhannah/typescript-helloworld/app.ts
@@ -1,5 +1,6 @@
 // https://www.typescripttutorial.net/typescript-tutorial/typescript-hello-world/
-
+// tsc app.ts
+// ^^ VSCode Live Server (neato)
 
 let message: string = 'Hello, TypeScript!';
 // create a new heading 1 element
