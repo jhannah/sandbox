a91827dac8af385b107f3326690108c79751cbce
Jay Hannah <jay.hannah@iinteractive.com>
2023-10-11 20:40:39 -0500

    Task 3

---
+++ jhannah/ML-crash-course/rep_with_a_feature_cross.py
@@ -0,0 +1,244 @@
+# https://colab.research.google.com/github/google/eng-edu/blob/main/ml/cc/exercises/representation_with_a_feature_cross.ipynb
+# pip install pydot
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+
+from matplotlib import pyplot as plt
+
+# The following lines adjust the granularity of reporting.
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+tf.keras.backend.set_floatx('float32')
+
+# Load the dataset
+# curl https://dl.google.com/mlcc/mledu-datasets/california_housing_train.csv > california_housing_train.csv
+train_df = pd.read_csv("california_housing_train.csv")
+# curl https://dl.google.com/mlcc/mledu-datasets/california_housing_test.csv > california_housing_test.csv
+test_df = pd.read_csv("california_housing_test.csv")
+
+# Scale the labels
+scale_factor = 1000.0
+# Scale the training set's label.
+train_df["median_house_value"] /= scale_factor
+
+# Scale the test set's label
+test_df["median_house_value"] /= scale_factor
+
+# Shuffle the examples
+train_df = train_df.reindex(np.random.permutation(train_df.index))
+
+# Keras Input tensors of float values.
+inputs = {
+  'latitude':
+    tf.keras.layers.Input(shape=(1,), dtype=tf.float32,
+                          name='latitude'),
+  'longitude':
+    tf.keras.layers.Input(shape=(1,), dtype=tf.float32,
+                          name='longitude')
+}
+
+def create_model(my_inputs, my_outputs, my_learning_rate):
+
+  model = tf.keras.Model(inputs=my_inputs, outputs=my_outputs)
+
+  # Construct the layers into a model that TensorFlow can execute.
+  model.compile(optimizer=tf.keras.optimizers.experimental.RMSprop(
+      learning_rate=my_learning_rate),
+      loss="mean_squared_error",
+      metrics=[tf.keras.metrics.RootMeanSquaredError()])
+
+  return model
+
+def train_model(model, dataset, epochs, batch_size, label_name):
+  """Feed a dataset into the model in order to train it."""
+
+  features = {name:np.array(value) for name, value in dataset.items()}
+  label = np.array(features.pop(label_name))
+  history = model.fit(x=features, y=label, batch_size=batch_size,
+                      epochs=epochs, shuffle=True)
+
+  # The list of epochs is stored separately from the rest of history.
+  epochs = history.epoch
+
+  # Isolate the mean absolute error for each epoch.
+  hist = pd.DataFrame(history.history)
+  rmse = hist["root_mean_squared_error"]
+
+  return epochs, rmse
+
+def plot_the_loss_curve(epochs, rmse):
+  """Plot a curve of loss vs. epoch."""
+
+  plt.figure()
+  plt.xlabel("Epoch")
+  plt.ylabel("Root Mean Squared Error")
+
+  plt.plot(epochs, rmse, label="Loss")
+  plt.legend()
+  plt.ylim([rmse.min()*0.94, rmse.max()* 1.05])
+  plt.show()
+
+# The following variables are the hyperparameters.
+learning_rate = 0.05
+epochs = 30
+batch_size = 100
+label_name = 'median_house_value'
+
+# The two Input layers are concatenated so they can be passed as a single
+# tensor to a Dense layer.
+preprocessing_layer = tf.keras.layers.Concatenate()(inputs.values())
+
+dense_output = layers.Dense(units=1, name='dense_layer')(preprocessing_layer)
+
+outputs = {
+  'dense_output': dense_output
+}
+
+# Create and compile the model's topography.
+my_model = create_model(inputs, outputs, learning_rate)
+
+# To view a PNG of this model's layers, uncomment the call to
+# `tf.keras.utils.plot_model` below. After running this code cell, click
+# the file folder on the left, then the `my_model.png` file.
+tf.keras.utils.plot_model(my_model, "my_model.png", show_shapes=True)
+
+# Train the model on the training set.
+epochs, rmse = train_model(my_model, train_df, epochs, batch_size, label_name)
+
+# Print out the model summary.
+my_model.summary(expand_nested=True)
+
+plot_the_loss_curve(epochs, rmse)
+
+print("\n: Evaluate the new model against the test set:")
+test_features = {name:np.array(value) for name, value in test_df.items()}
+test_label = np.array(test_features.pop(label_name))
+my_model.evaluate(x=test_features, y=test_label, batch_size=batch_size)
+
+
+'''
+Task 2
+resolution_in_degrees = 1.0
+
+# Create a list of numbers representing the bucket boundaries for latitude.
+latitude_boundaries = list(np.arange(int(min(train_df['latitude'])),
+                                     int(max(train_df['latitude'])),
+                                     resolution_in_degrees))
+print("latitude boundaries: " + str(latitude_boundaries))
+
+# Create a Discretization layer to separate the latitude data into buckets.
+latitude = tf.keras.layers.Discretization(
+    bin_boundaries=latitude_boundaries,
+    name='discretization_latitude')(inputs.get('latitude'))
+
+# Number of categories is the length of latitude_boundaries plus one.
+latitude = tf.keras.layers.CategoryEncoding(
+    num_tokens=len(latitude_boundaries) + 1,
+    output_mode='one_hot',
+    name='category_encoding_latitude')(latitude)
+
+# Create a list of numbers representing the bucket boundaries for longitude.
+longitude_boundaries = list(np.arange(int(min(train_df['longitude'])),
+                                      int(max(train_df['longitude'])),
+                                      resolution_in_degrees))
+
+print("longitude boundaries: " + str(longitude_boundaries))
+
+# Create a Discretization layer to separate the longitude data into buckets.
+longitude = tf.keras.layers.Discretization(
+    bin_boundaries=longitude_boundaries,
+    name='discretization_longitude')(inputs.get('longitude'))
+
+# Number of categories is the length of longitude_boundaries plus one.
+longitude = tf.keras.layers.CategoryEncoding(
+    num_tokens=len(longitude_boundaries) + 1,
+    output_mode='one_hot',
+    name='category_encoding_longitude')(longitude)
+
+# Concatenate latitude and longitude into a single tensor as input for the Dense layer.
+concatenate_layer = tf.keras.layers.Concatenate()([latitude, longitude])
+
+dense_output = layers.Dense(units=1, name='dense_layer')(concatenate_layer)
+
+# Define an output dictionary we'll send to the model constructor.
+outputs = {
+  'dense_output': dense_output
+}
+
+# The following variables are the hyperparameters.
+learning_rate = 0.04
+epochs = 35
+
+# Build the model.
+my_model = create_model(inputs, outputs, learning_rate)
+
+# Train the model on the training set.
+epochs, rmse = train_model(my_model, train_df, epochs, batch_size, label_name)
+
+# Print out the model summary.
+my_model.summary(expand_nested=True)
+
+plot_the_loss_curve(epochs, rmse)
+
+print("\n: Evaluate the new model against the test set:")
+my_model.evaluate(x=test_features, y=test_label, batch_size=batch_size)
+'''
+
+# Task 3
+resolution_in_degrees = 1.0
+
+# Create a list of numbers representing the bucket boundaries for latitude.
+latitude_boundaries = list(np.arange(int(min(train_df['latitude'])),
+                                     int(max(train_df['latitude'])),
+                                     resolution_in_degrees))
+
+# Create a Discretization layer to separate the latitude data into buckets.
+latitude = tf.keras.layers.Discretization(
+    bin_boundaries=latitude_boundaries,
+    name='discretization_latitude')(inputs.get('latitude'))
+
+# Create a list of numbers representing the bucket boundaries for longitude.
+longitude_boundaries = list(np.arange(int(min(train_df['longitude'])),
+                                      int(max(train_df['longitude'])),
+                                      resolution_in_degrees))
+
+# Create a Discretization layer to separate the longitude data into buckets.
+longitude = tf.keras.layers.Discretization(
+    bin_boundaries=longitude_boundaries,
+    name='discretization_longitude')(inputs.get('longitude'))
+
+# Cross the latitude and longitude features into a single one-hot vector.
+feature_cross = tf.keras.layers.HashedCrossing(
+    num_bins=len(latitude_boundaries) * len(longitude_boundaries),
+    output_mode='one_hot',
+    name='cross_latitude_longitude')([latitude, longitude])
+
+dense_output = layers.Dense(units=1, name='dense_layer')(feature_cross)
+
+# Define an output dictionary we'll send to the model constructor.
+outputs = {
+  'dense_output': dense_output
+}
+
+# The following variables are the hyperparameters.
+learning_rate = 0.04
+epochs = 35
+
+# Build the model, this time passing in the feature_cross_feature_layer:
+my_model = create_model(inputs, outputs, learning_rate)
+
+# Train the model on the training set.
+epochs, rmse = train_model(my_model, train_df, epochs, batch_size, label_name)
+
+# Print out the model summary.
+my_model.summary(expand_nested=True)
+
+plot_the_loss_curve(epochs, rmse)
+
+print("\n: Evaluate the new model against the test set:")
+my_model.evaluate(x=test_features, y=test_label, batch_size=batch_size)
+
+
