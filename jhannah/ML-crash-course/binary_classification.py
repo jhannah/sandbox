cf6fe95acede57ce3d4007b013c11f9d7834aac9
Jay Hannah <jay.hannah@iinteractive.com>
2023-10-12 21:13:54 -0500

    task 5

--- jhannah/ML-crash-course/binary_classification.py
+++ jhannah/ML-crash-course/binary_classification.py
@@ -161,12 +161,25 @@ label = np.array(features.pop(label_name))
 my_model.evaluate(x = features, y = label, batch_size=batch_size)
 '''
 
-# Task 3
+'''
+# Task 3, 4
 # The following variables are the hyperparameters.
 learning_rate = 0.001
 epochs = 20
 batch_size = 100
-classification_threshold = 0.35
+classification_threshold = 0.35 # accuracy: 0.8098 - precision: 0.6206 - recall: 0.6136
+classification_threshold = 0.5  # accuracy: 0.8340 - precision: 0.7596 - recall: 0.4909
+classification_threshold = 0.2  # accuracy: 0.7309 - precision: 0.4762 - recall: 0.7740
+classification_threshold = 0.4  # accuracy: 0.8204 - precision: 0.6610 - recall: 0.5771
+classification_threshold = 0.3  # accuracy: 0.7921 - precision: 0.5722 - recall: 0.6640
+classification_threshold = 0.52 # accuracy: 0.8363 - precision: 0.7886 - recall: 0.4709
+# A `classification_threshold` of slightly over 0.5
+# appears to produce the highest accuracy (about 83%).
+# Raising the `classification_threshold` to 0.9 drops
+# accuracy by about 5%.  Lowering the
+# `classification_threshold` to 0.3 drops accuracy by
+# about 3%.
+
 label_name = "median_house_value_is_high"
 
 # Modify the following definition of METRICS to generate
@@ -190,5 +203,31 @@ epochs, hist = train_model(my_model, train_df_norm, epochs,
 # Plot metrics vs. epochs
 list_of_metrics_to_plot = ['accuracy', 'precision', 'recall']
 plot_curve(epochs, hist, list_of_metrics_to_plot)
+'''
 
+# Task 5
+# The following variables are the hyperparameters.
+learning_rate = 0.001
+epochs = 20
+batch_size = 100
+label_name = "median_house_value_is_high"
+
+# AUC is a reasonable "summary" metric for
+# classification models.
+# Here is the updated definition of METRICS to
+# measure AUC:
+METRICS = [
+      tf.keras.metrics.AUC(num_thresholds=100, name='auc'),
+]
+
+# Establish the model's topography.
+my_model = create_model(inputs, learning_rate, METRICS)
+
+# Train the model on the training set.
+epochs, hist = train_model(my_model, train_df_norm, epochs,
+                           label_name, batch_size)
+
+# Plot metrics vs. epochs
+list_of_metrics_to_plot = ['auc']
+plot_curve(epochs, hist, list_of_metrics_to_plot)
 
