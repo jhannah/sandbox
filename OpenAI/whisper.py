6da4e2243855838c17c517d8ed230c2fb3b0ab76
Jay Hannah <jay.hannah@iinteractive.com>
2024-03-12 07:11:01 -0500

    new version of whisper requires more python mojo

--- OpenAI/whisper.py
+++ OpenAI/whisper.py
@@ -1,18 +1,28 @@
 # https://platform.openai.com/docs/tutorials/meeting-minutes
-# pip3 install openai
+# python -m venv env
+# source env/bin/activate
+# pip install openai
+# pip install python-docx
 
 # Gotta split into ~10MB files because 25MB is the theoretical max
 # ffmpeg -i /Users/jhannah/Dropbox/Public/jay_flaunts/043.mp3 -f segment -segment_time 1200 -c copy ./out%03d.mp3
 # In vim: gq to add hard word wrapping
 
-import openai
+from openai import OpenAI
+
+client = OpenAI()  # defaults to os.environ.get("OPENAI_API_KEY")
+
+from docx import Document
 
 def transcribe_audio(audio_file_path):
   with open(audio_file_path, 'rb') as audio_file:
-    transcription = openai.Audio.transcribe("whisper-1", audio_file)
-  return transcription['text']
+    transcription = client.audio.transcriptions.create(
+      model="whisper-1",
+      file=audio_file,
+    )
+  return transcription
 
-text = transcribe_audio("out003.mp3")
-print(text)
+transcription = transcribe_audio("out001.mp3")
+print(transcription.text)
 
 
