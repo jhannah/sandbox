2ad4ddf8f4c6c9dba5d8556bccf43f6ed34995f8
Jay Hannah <jay.hannah@iinteractive.com>
2025-01-20 13:21:47 -0600

    rough-out

--- otrr/have_gun_will_travel.pl
+++ otrr/have_gun_will_travel.pl
@@ -1,5 +1,6 @@
 #! env perl
 
+use 5.40.0;
 use Template;
 
 my $tt = Template->new({
@@ -7,12 +8,44 @@ my $tt = Template->new({
   INTERPOLATE  => 1,
 }) || die "$Template::ERROR\n";
 
+my @episodes;
+while (<DATA>) {
+  chomp;
+  my $ep = {};
+  if (/Introduction/) {
+    push @episodes, {
+      filename  => $_,
+      title     => "OTRR Introduction",
+      pub_date  => "19 May 2024 00:20:34 GMT",
+      ep_number => 0,
+    };
+  } else {
+    my $filename = $_;
+    # Have Gun—Will Travel 1958-11-23 (001) Strange Vendetta.flac
+    my ($pub_date, $ep_number, $title) = ($filename =~ /Have Gun—Will Travel ([\d\-]+) \((\d+)\) ([\w\s]+)/);
+    $ep_number =~ s/^0+//;
+    push @episodes, {
+      filename  => $filename,
+      title     => $title,
+      pub_date  => $pub_date,
+      ep_number => $ep_number,
+    };
+  }
+}
 my $vars = {
-  name     => 'Count Edward van Halen',
-  debt     => '3 riffs and a solo',
-  deadline => 'the next chorus',
+  title      => 'Have Gun Will Travel',
+  owner_name => 'Old-Time Radio Researchers',
+  image      => 'https://archive.org/download/OTRR_Maintained_Have_Gun_Will_Travel/OTRR_Maintained_Have_Gun_Will_Travel.jpg',
+  link       => 'https://www.otrr.org/',
+  summary    => 'Radio program that aired 1958-1960. Long description here: https://archive.org/details/OTRR_Maintained_Have_Gun_Will_Travel',
+  per_ep_root_url => 'https://archive.org/download/OTRR_Have_Gun_Singles',
+  episodes   => [ @episodes ],
 };
 
 $tt->process('feed.rss.tt', $vars, 'have_gun_will_travel.rss')
   || die $tt->error(), "\n";
 
+__DATA__
+00 OTRR Introduction.mp3
+Have Gun—Will Travel 1958-11-23 (001) Strange Vendetta.flac
+Have Gun—Will Travel 1958-11-30 (002) Road to Wickenburg.flac
