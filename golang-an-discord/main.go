e6c8d47a943805918f84deffcf9cba2e9c473f78
Jay Hannah <jay@jays.net>
2025-05-06 18:29:10 -0500

    remove old debugging

--- golang-an-discord/main.go
+++ golang-an-discord/main.go
@@ -42,20 +42,6 @@ func main() {
 	discordBotToken := os.Getenv("DISCORD_BOT_TOKEN")
 	discordChannelID := os.Getenv("DISCORD_CHANNEL_ID")
 
-	// You can get these strings by typing \:dfflirt: into Discord
-	messages := []string{"<:dfflirt:796580268374884372>", "<:dfflirt1:796582372078125097>", "<:dfflirt2:796582435332161567>"}
-	messages = []string{"Let's go fetch from AN..."}
-	messages = []string{}
-	for _, message := range messages {
-		fmt.Println(message)
-		err := postToDiscordChannel(discordBotToken, discordChannelID, message)
-		if err != nil {
-			fmt.Println("Error posting to Discord:", err)
-		} else {
-			fmt.Println("Message posted successfully!")
-		}
-	}
-
 	allActions := []Action{}
 
 	for _, key := range actionNetworkAPIKeys {
@@ -67,9 +53,7 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			for _, action := range actions {
-				allActions = append(allActions, action)
-			}
+			allActions = append(allActions, actions...)
 			if nextPage == "" {
 				break
 			}
@@ -105,21 +89,6 @@ func main() {
 	}
 
 	fmt.Print("All done. Exiting.\n")
-
-	/*
-		action := Action{
-			ID:          "1",
-			Title:       "super party",
-			Description: "it's lit, yo",
-			StartTime:   time.Now().Add(24 * time.Hour),
-			EndTime:     time.Now().Add(26 * time.Hour),
-			Location:    "your mom's house",
-		}
-		err := createDiscordEvent(action)
-		if err != nil {
-			fmt.Println("Error creating Discord event:", err)
-		}
-	*/
 }
 
 func prettyPrintJSON(raw []byte) {
