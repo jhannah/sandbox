7f0268f3032793e7c635c4b2630a746d145d67d6
Jay Hannah <jay@jays.net>
2025-06-18 21:42:32 -0500

    aws_glue_catalog_table added, working

--- cp/main.tf
+++ cp/main.tf
@@ -66,3 +66,41 @@ resource "aws_glue_crawler" "csv_crawler" {
   schedule = null  # on-demand; remove or change to run automatically
 }
 
+resource "aws_glue_catalog_table" "user_events" {
+  name          = "user_events"
+  database_name = aws_glue_catalog_database.cp_data_lake_db.name
+  table_type    = "EXTERNAL_TABLE"
+
+  parameters = {
+    "classification" = "csv"
+    "compressionType" = "none"
+    "typeOfData" = "file"
+  }
+
+  storage_descriptor {
+    columns {
+      name = "user_id"
+      type = "string"
+    }
+    columns {
+      name = "event_type"
+      type = "string"
+    }
+    columns {
+      name = "event_timestamp"
+      type = "string"
+    }
+    location      = "s3://${aws_s3_bucket.cp-s3.id}/user_events.csv"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+    ser_de_info {
+      name                  = "user_events"
+      serialization_library = "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe"
+      parameters = {
+        "field.delim" = ","
+        "skip.header.line.count" = "1"
+      }
+    }
+  }
+}
+
